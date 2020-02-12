1bede4335100d55196f2b628077eec77ffafde9e
ashish-aarya <arya.ashish229@gmail.com>
2020-02-12 23:41:12 +0530

    Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-runApp (MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp (home: Text('Hello'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My FIrst app'),
+        ),
+        body: Text('this is the def. text'),
+      ),
+    );
   }
 }
