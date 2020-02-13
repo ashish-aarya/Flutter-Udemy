1375f72249134f9be7df6b4506ebced55ee584d5
ashish-aarya <arya.ashish229@gmail.com>
2020-02-13 23:41:22 +0530

    Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    var questions =
+    ['What\'s your fav.color?','What\'s your fav. animal',];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My FIrst app'),
         ),
-        body: Text('this is the def. text'),
+        body:Column(
+          children: [Text('ques') ,
+          RaisedButton(child: Text('Answer 1'),onPressed: null,),
+           RaisedButton(child: Text('Answer 1'),onPressed: null,),
+            RaisedButton(child: Text('Answer 1'),onPressed: null,)],) ,
       ),
     );
   }
